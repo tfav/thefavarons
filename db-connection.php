557e3117c6ac6cd41e722c146c06158ab5fc5cff
Travis Favaron <travisfavaron@gmail.com>
2015-07-28 19:31:31 -0500

    more db cleanup

--- db-connection.php
+++ db-connection.php
@@ -1,9 +1,16 @@
 <?php
 
 $dbhost = $_ENV['DATABASE_SERVER'];
+
+//Local
 $dbuser = "root";
 $dbpass = "root";
 $dbtable = "thefavarons";
+
+// Production
+// $dbuser = "db140168_wedding";
+// $dbpass = "f]8+xtT4Rqmr";
+// $dbtable = "thefavarons";
 
 
 $link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbtable);
