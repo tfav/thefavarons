a94ecc55d31f86126c739737d8c980659f6d5418
Travis Favaron <travisfavaron@gmail.com>
2015-07-29 17:43:45 -0500

    neuter rsvp until i can figure out db

--- rsvp.php
+++ rsvp.php
@@ -6,13 +6,14 @@
   </div>
   </div>
   <div class="content content-centered">
-    <h5>Please enter the name on your invitation (without the Mr. or Mrs.)</h5>
+    <h5>Hang tight, you'll be able to RSVP soon!</h5>
+    <!-- <h5>Please enter the name on your invitation (without the Mr. or Mrs.)</h5>
     <form class="form-inline" action="rsvp-form" method="post" data-toggle="validator">
       <div class="form-group">
         <input type="text" class="form-control input-lg" name="name" placeholder="Travis Favaron" required>
       </div>
       <button type="submit" class="btn btn-default btn-lg">Find</button>
     </form>
-    <p>If you are having trouble finding your invitation, please <a href='contact'>contact us</a>.</p>
+    <p>If you are having trouble finding your invitation, please <a href='contact'>contact us</a>.</p> -->
   </div>
 <?php require_once('footer.php') ?>
