face350f6aa1aa44bb9a38ed251e17fc1c3f200b
Travis Favaron <travisfavaron@gmail.com>
2015-07-30 09:30:49 -0500

    turn on rsvp

--- nav.php
+++ nav.php
@@ -6,7 +6,7 @@
         <li><a href="https://www.bedbathandbeyond.com:443/store/giftregistry/view_registry_guest.jsp?registryId=542195596&eventType=Wedding&pwsurl=" target="_blank">Registry</a></li>
         <li><a href="contact">Contact</a></li>
         <li><a href="faq">FAQ</a></li>
-        <!-- <li><a href="lookup.php">RSVP</a></li> -->
+        <li><a href="rsvp">RSVP</a></li>
         <li><a href="/">Home</a></li>
       </ul>
     </nav>
